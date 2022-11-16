e88146b6f8c5e96e42fdf7e51cc7a2f4f6871ae5
Ted Isaac <ndeited@gmail.com>
2022-11-16 16:41:57 +0300

    RecyclerView, Adapter and Fragment Implementation

---
+++ app/src/main/java/com/example/inventory/InventoryViewModel.kt
@@ -0,0 +1,52 @@
+package com.example.inventory
+
+import androidx.lifecycle.*
+import com.example.inventory.data.Item
+import com.example.inventory.data.ItemDao
+import kotlinx.coroutines.launch
+
+class InventoryViewModel(private val itemDao: ItemDao) : ViewModel() {
+
+    val allItems : LiveData<List<Item>> = itemDao.getItems().asLiveData()
+
+    private fun insertItem(item: Item){
+        viewModelScope.launch {
+            itemDao.insert(item)
+        }
+    }
+
+    private fun getNewEntryItem(itemName: String, itemPrice: String, itemCount: String) : Item{
+        return Item(
+            itemName = itemName,
+            itemPrice = itemPrice.toDouble(),
+            quantityInStock = itemCount.toInt()
+        )
+    }
+
+    fun isEntryValid(itemName: String, itemPrice: String, itemCount: String) : Boolean{
+        if (itemName.isBlank() || itemPrice.isBlank() || itemCount.isBlank()){
+            return false
+        }
+        return true
+    }
+
+    fun addNewItem(itemName: String, itemPrice: String, itemCount: String) {
+        val newItem = getNewEntryItem(itemName, itemPrice, itemCount)
+        insertItem(newItem)
+    }
+
+    fun retrieveItem(id: Int): LiveData<Item> {
+        return itemDao.getItem(id).asLiveData()
+    }
+
+}
+
+class InventoryViewModelFactory(private val itemDao: ItemDao) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(InventoryViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return InventoryViewModel(itemDao) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
